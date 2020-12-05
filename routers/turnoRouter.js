67a96284940b439e8228de9f40a5883356626e3e
TongasHub <ferreyragaston40@gmail.com>
2020-12-05 19:50:53 -0300

    turnoRoute arreglado

--- routers/turnoRouter.js
+++ routers/turnoRouter.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const turnoController = require('../controllers/turnoController');
 
 // endpoints triviales
+router.route('/aceptados').get(turnoController.getTurnosAceptados);
+router.route('/ocupados').get(turnoController.getFechasOcupadas);
 
 router
   .route('/:id')
@@ -10,10 +12,5 @@ router
   .delete(turnoController.deleteOne);
 
 router.route('/').get(turnoController.getAll).post(turnoController.createOne);
-
-// endpoints no triviales
-
-router.route('/aceptados').get(turnoController.getTurnosAceptados);
-router.route('/ocupados').get(turnoController.getFechasOcupadas);
 
 module.exports = router;
