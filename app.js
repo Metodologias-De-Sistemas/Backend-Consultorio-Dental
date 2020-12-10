e0b5e8a900b7312fc2e05f35939016813fbe5ff0
maurirlz <mauribenitez95@gmail.com>
2020-12-10 13:19:13 -0300

    Improved bodyParser previous limit to account for image uploads

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 // Informacion / Opciones  / Configuracion
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(morgan('dev'));
 app.use(extraerToken);
 
